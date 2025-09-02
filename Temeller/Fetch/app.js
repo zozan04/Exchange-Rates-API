58c6063adc2f50fa93ebf19e88469fcfbe81babf
ZDuman <zozanduman3030@gmail.com>
2025-09-02 13:23:29 +0300

    js Örnekler submodule tamamen kaldırıldı

--- Temeller/Fetch/app.js
+++
@@ -1,9 +0,0 @@
-function getTextFile(){
-
- fetch("example.txt")
- .then(response=>response.text())
- .then(data=>console.log(data))
- .catch(err=>console.log(err));
- 
-}
-getTextFile();
--- Temeller/operatorler/app.js
+++
@@ -1,7 +0,0 @@
-let value;
-
-value=Math.PI;
-value=Math.round(3.6);
-value=Math.round(3.5);
-value=Math.round(3.2);
-console.log(value);
